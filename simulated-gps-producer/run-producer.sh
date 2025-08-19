409a4bba6aa25f7bddd811117f3b0af02ea3eea2
kenichsberg <kn1017356@gmail.com>
2025-08-19 20:24:32 +0300

    fix: let spark-producer container fail correctly

--- simulated-gps-producer/run-producer.sh
+++ simulated-gps-producer/run-producer.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
-start_spark() {
-  echo "[ENTRYPOINT] Starting Spark..."
-  /opt/bitnami/spark/bin/spark-submit \
-    --conf spark.sql.files.ignoreCorruptFiles=true \
-    --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.6 \
-    --py-files ./simulated-gps-producer.zip main.py \
-    --checkpoint /checkpoints &
-  SPARK_PID=$!
-}
+# Start Spark in background
+/opt/bitnami/spark/bin/spark-submit \
+  --conf spark.sql.files.ignoreCorruptFiles=true \
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.6 \
+  --py-files ./simulated-gps-producer.zip main.py \
+  --checkpoint /checkpoints &
+SPARK_PID=$!
 
-start_feeder() {
-  echo "[ENTRYPOINT] Starting file feeder..."
-  python /app/src/dags/feed_ingestion.py &
-  FEEDER_PID=$!
-}
+# healthcheck
+for i in {1..30}; do
+  if nc -z localhost 4040; then
+    echo "[ENTRYPOINT] Spark is up"
+    break
+  fi
+  echo "[ENTRYPOINT] Waiting for Spark..."
+  sleep 2
+done
 
-start_spark
-sleep 20
-#start_feeder
-echo "[ENTRYPOINT] Starting file feeder..."
-uv run python -m src.dags.feed_ingestion
+# Start feeder in background
+uv run python -m src.dags.feed_ingestion &
+FEEDER_PID=$!
+
+# Wait for both
+wait -n $SPARK_PID $FEEDER_PID
+exit $?
